a89893be0f141d7df8b1489a492169c3eaf704d2
= <sean.fioritto@gmail.com>
2014-03-06 11:34:02 -0600

    installed autoprefixer

--- Gruntfile.js
+++ Gruntfile.js
@@ -74,6 +74,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-autoprefixer');
 
   // Default task.
   grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
